af0f05b07406be987d91fb948def8b14013f2335
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-15 19:34:45 +0300

    Create Odev8-3.sql

---
+++ Odev8-3.sql
@@ -0,0 +1,6 @@
+--Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım. (?)
+UPDATE employee SET name = 'Merih', birthday = '10-09-2020', email = 'merh1@gam.com' WHERE id =50;
+UPDATE employee SET name = 'Merihcan', birthday = '10-09-2001', email = 'merh2@gam.com' WHERE id =14;
+UPDATE employee SET name = 'Merihnur', birthday = '10-09-2010', email = 'merh3@gam.com' WHERE id =23;
+UPDATE employee SET name = 'Merihsu', birthday = '10-09-2002', email = 'merh4@gam.com' WHERE id =20;
+UPDATE employee SET name = 'Merihgül', birthday = '10-09-2011', email = 'merh5@gam.com' WHERE id =40;
